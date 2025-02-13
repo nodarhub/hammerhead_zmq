35890fc6529afd71755c45932f7446c36e14740b
sabyanodar <sabya@nodarsensor.com>
2025-02-13 14:58:33 +0100

    Interface cleaned up

--- examples/topbot_publisher/src/topbot_publisher.cpp
+++ examples/topbot_publisher/src/topbot_publisher.cpp
@@ -1,8 +1,9 @@
+#include <atomic>
+#include <csignal>
 #include <chrono>
 #include <filesystem>
 #include <iostream>
 #include <nodar/zmq/image.hpp>
-#include <nodar/zmq/opencv_utils.hpp>
 #include <nodar/zmq/publisher.hpp>
 #include <opencv2/opencv.hpp>
 #include <string>
@@ -10,6 +11,12 @@
 #include <vector>
 
 constexpr auto FRAME_RATE = 10;
+std::atomic_bool running{true};
+
+void signalHandler(int) {
+    std::cerr << "SIGINT or SIGTERM received. Exiting..." << std::endl;
+    running = false;
+}
 
 std::vector<std::string> getImageFiles(const std::string& folder) {
     std::vector<std::string> image_files;
@@ -22,24 +29,43 @@ std::vector<std::string> getImageFiles(const std::string& folder) {
     return image_files;
 }
 
+class TopbotPublisher {
+public:
+    TopbotPublisher() : publisher(nodar::zmq::IMAGE_TOPICS[6], "") {}
+
+    void publishImage(const cv::Mat& img, uint64_t timestamp, uint64_t frame_id) {
+        auto buffer = publisher.getBuffer();
+        buffer->resize(nodar::zmq::StampedImage::msgSize(img.rows, img.cols, img.type()));
+        nodar::zmq::StampedImage::write(buffer->data(), timestamp, frame_id, img.rows, img.cols, img.type(), img.data);
+        publisher.send(buffer);
+    }
+
+private:
+    nodar::zmq::Publisher<nodar::zmq::StampedImage> publisher;
+};
+
 int main(int argc, char* argv[]) {
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);
+
     if (argc < 2) {
-        std::cerr << "Usage: topbot_publisher <topbot_image_folder>" << std::endl;
+        std::cerr << "Usage: topbot_publisher <image_folder>" << std::endl;
         return EXIT_FAILURE;
     }
 
-    const std::filesystem::path folder{argv[1]};
-    auto image_files = getImageFiles(folder);
+    auto image_files = getImageFiles(argv[1]);
     if (image_files.empty()) {
-        std::cerr << "No images found in folder: " << folder << std::endl;
+        std::cerr << "No images found in folder." << std::endl;
         return EXIT_FAILURE;
     }
 
-    nodar::zmq::Publisher<nodar::zmq::StampedImage> publisher(nodar::zmq::IMAGE_TOPICS[6], "");
+    TopbotPublisher publisher;
     uint64_t frame_id = 0;
 
-    while (true) {
+    while (running) {
         for (const auto& file : image_files) {
+            if (!running) break;
+
             cv::Mat img = cv::imread(file, cv::IMREAD_COLOR);
             if (img.empty()) {
                 std::cerr << "Failed to load image: " << file << std::endl;
@@ -47,19 +73,16 @@ int main(int argc, char* argv[]) {
             }
 
             uint64_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(
-                                     std::chrono::system_clock::now().time_since_epoch())
-                                     .count();
+                                     std::chrono::system_clock::now().time_since_epoch()).count();
 
-            auto buffer = publisher.getBuffer();
-            buffer->resize(nodar::zmq::StampedImage::msgSize(img.rows, img.cols, img.type()));
-            nodar::zmq::StampedImage::write(buffer->data(), timestamp, frame_id, img.rows, img.cols, img.type(),
-                                            img.data);
-            publisher.send(buffer);
-
+            publisher.publishImage(img, timestamp, frame_id);
             std::cout << "Published frame " << frame_id << " from " << file << std::endl;
             frame_id++;
 
             std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FRAME_RATE));
         }
     }
+
+    std::cout << "Publisher stopped." << std::endl;
+    return EXIT_SUCCESS;
 }
