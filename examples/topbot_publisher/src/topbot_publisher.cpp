864e3ebfbafc2bda108cdbd18114fc7b5bf5baad
sabyanodar <sabya@nodarsensor.com>
2025-02-19 11:03:42 +0100

    Modularization and namespacing

--- examples/topbot_publisher/src/topbot_publisher.cpp
+++ examples/topbot_publisher/src/topbot_publisher.cpp
@@ -1,12 +1,8 @@
+#include "topbot_publisher.hpp"
+
 #include <atomic>
 #include <chrono>
 #include <csignal>
-#include <filesystem>
-#include <iostream>
-#include <nodar/zmq/image.hpp>
-#include <nodar/zmq/publisher.hpp>
-#include <opencv2/opencv.hpp>
-#include <string>
 #include <thread>
 
 #include "get_files.hpp"
@@ -18,56 +14,6 @@ void signalHandler(int) {
     std::cerr << "SIGINT or SIGTERM received. Exiting..." << std::endl;
     running = false;
 }
-
-std::string depthToString(const int& depth) {
-    switch (depth) {
-        case CV_8U:
-            return "CV_8U (8-bit unsigned)";
-        case CV_8S:
-            return "CV_8S (8-bit signed)";
-        case CV_16U:
-            return "CV_16U (16-bit unsigned)";
-        case CV_16S:
-            return "CV_16S (16-bit signed)";
-        case CV_32S:
-            return "CV_32S (32-bit signed)";
-        case CV_32F:
-            return "CV_32F (32-bit float)";
-        case CV_64F:
-            return "CV_64F (64-bit float)";
-        default:
-            return "Unknown depth";
-    }
-}
-
-class TopbotPublisher {
-public:
-    TopbotPublisher() : publisher(nodar::zmq::IMAGE_TOPICS[6], "") {}
-
-    bool publishImage(const cv::Mat& img, const uint64_t& timestamp, const uint64_t& frame_id) {
-        if (img.empty()) {
-            std::cerr << "Failed to load image" << std::endl;
-            return false;
-        }
-        const auto depth = img.depth();
-        const auto channels = img.channels();
-
-        if (!((depth == CV_8U || depth == CV_16U) && channels == 3)) {
-            std::cerr << "Skipping unsupported image type: depth=" << depthToString(depth) << ", channels=" << channels
-                      << std::endl;
-            return false;
-        }
-
-        auto buffer = publisher.getBuffer();
-        buffer->resize(nodar::zmq::StampedImage::msgSize(img.rows, img.cols, img.type()));
-        nodar::zmq::StampedImage::write(buffer->data(), timestamp, frame_id, img.rows, img.cols, img.type(), img.data);
-        publisher.send(buffer);
-        return true;
-    }
-
-private:
-    nodar::zmq::Publisher<nodar::zmq::StampedImage> publisher;
-};
 
 int main(int argc, char* argv[]) {
     signal(SIGINT, signalHandler);
@@ -84,7 +30,7 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    TopbotPublisher publisher;
+    nodar::zmq::TopbotPublisher publisher;
     auto frame_id = 0;
 
     while (running) {
