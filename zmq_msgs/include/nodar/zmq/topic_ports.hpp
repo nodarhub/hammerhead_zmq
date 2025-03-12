0b42a2bdbc10dba07b1676f71abfb5bdeed59483
sabyanodar <sabya@nodarsensor.com>
2025-03-12 14:30:25 +0100

    Reintroduce back the topic for future

--- zmq_msgs/include/nodar/zmq/topic_ports.hpp
+++ zmq_msgs/include/nodar/zmq/topic_ports.hpp
@@ -22,7 +22,8 @@ constexpr std::array<Topic, 7> IMAGE_TOPICS{{{"nodar/left/image_raw", 9800},  //
                                              {"nodar/left/image_rect", 9802},  //
                                              {"nodar/right/image_rect", 9803},  //
                                              {"nodar/disparity", 9804},  //
-                                             {"nodar/color_blended_depth/image_raw", 9805}}};
+                                             {"nodar/color_blended_depth/image_raw", 9805},  //
+                                             {"nodar/topbot_raw", 9813}}};
 
 constexpr Topic SOUP_TOPIC{"nodar/point_cloud_soup", 9806};
 
