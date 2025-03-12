5d7cb4d81232394774f5231783a61323dbf88382
sabyanodar <sabya@nodarsensor.com>
2025-03-12 11:07:47 +0100

    Adding external image topic mappings

--- zmq_msgs/include/nodar/zmq/topic_ports.hpp
+++ zmq_msgs/include/nodar/zmq/topic_ports.hpp
@@ -12,6 +12,11 @@ struct Topic {
     uint16_t port;
 };
 
+// External Image Topics
+constexpr std::array<Topic, 2> EXTERNAL_IMAGE_TOPICS{{{"external/topbot_bgr", 5000},  //
+                                                      {"external/topbot_bayer_bggr", 5001}}};
+
+// Image topics internal to Hammerhead
 constexpr std::array<Topic, 7> IMAGE_TOPICS{{{"nodar/left/image_raw", 9800},  //
                                              {"nodar/right/image_raw", 9801},  //
                                              {"nodar/left/image_rect", 9802},  //
@@ -33,23 +38,6 @@ constexpr Topic RECORDING_TOPIC{"nodar/recording", 9811};
 constexpr Topic OBSTACLE_TOPIC{"nodar/obstacle", 9812};
 
 constexpr Topic WAIT_TOPIC{"nodar/wait", 9814};
-
-// Function to retrieve reserved ports dynamically
-inline auto getReservedPorts() {
-    std::set<uint16_t> reserved_ports;
-    for (const auto& topic : nodar::zmq::IMAGE_TOPICS) {
-        reserved_ports.insert(topic.port);
-    }
-    reserved_ports.insert(nodar::zmq::SOUP_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::CAMERA_EXPOSURE_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::CAMERA_GAIN_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::POINT_CLOUD_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::POINT_CLOUD_RGB_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::RECORDING_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::OBSTACLE_TOPIC.port);
-    reserved_ports.insert(nodar::zmq::WAIT_TOPIC.port);
-    return reserved_ports;
-}
 
 }  // namespace zmq
 }  // namespace nodar
