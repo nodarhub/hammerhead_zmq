f2a4d8d6aae86a174ed590ae62c7c2d9002a6ae2
sabyanodar <sabya@nodarsensor.com>
2025-12-10 16:20:13 +0100

    Setup file

---
+++ examples/python/velocity_publisher/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = "velocity_publisher"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=["setuptools"],
+    zip_safe=False,
+    maintainer="nodar",
+    maintainer_email="support@nodarsensor.com",
+    description="This example shows how to publish velocity data to Hammerhead.",
+    license="NODAR Limited Copyright License",
+    license_files=["LICENSE"],
+    project_urls={
+        "License": "https://github.com/nodarhub/hammerhead_zmq/blob/main/LICENSE",
+    },
+    entry_points={
+        "console_scripts": [
+            f"{package_name} = {package_name}.{package_name}:main",
+        ],
+    },
+)
