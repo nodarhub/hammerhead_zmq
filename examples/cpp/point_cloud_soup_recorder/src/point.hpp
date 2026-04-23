5197a7eb7aa1af1b41a51da4a93c1cf414f8b724
sabyanodar <sabya@nodarsensor.com>
2026-04-23 12:55:12 +0200

    Add in constructor for PointXYZRGB

--- examples/cpp/point_cloud_soup_recorder/src/point.hpp
+++ examples/cpp/point_cloud_soup_recorder/src/point.hpp
@@ -10,4 +10,8 @@ struct PointXYZRGB {
     uint8_t g;
     uint8_t b;
     uint8_t a = 1;
+
+    PointXYZRGB() = default;
+    PointXYZRGB(float x_, float y_, float z_, uint8_t r_, uint8_t g_, uint8_t b_)
+        : x(x_), y(y_), z(z_), r(r_), g(g_), b(b_) {}
 };
